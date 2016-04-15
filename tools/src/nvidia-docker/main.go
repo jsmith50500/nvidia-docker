6d5acf7eb3a18296667956b946132fd52a052727
jsmith50500 <jsmith50500@users.noreply.github.com>
2016-04-15 17:13:55 +0100

    Update main.go

--- tools/src/nvidia-docker/main.go
+++ tools/src/nvidia-docker/main.go
@@ -10,8 +10,8 @@ import (
 	"runtime"
 	"runtime/debug"
 
-	"github.com/NVIDIA/nvidia-docker/tools/src/docker"
-	"github.com/NVIDIA/nvidia-docker/tools/src/nvidia"
+	"github.com/jsmith50500/nvidia-docker/tools/src/docker"
+	"github.com/jsmith50500/nvidia-docker/tools/src/nvidia"
 )
 
 var (
